6c4ab49b9ec372f7fae1ff0492e9756cbe3ee042
iamjumshaid <iamjumshaidkhan@gmail.com>
2024-01-10 14:30:33 +0100

    🐛 Fix lpr command

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ load_dotenv()
 
 project_folder = os.path.dirname(os.path.realpath(__file__))
 printables_folder = os.path.join(project_folder, "printables")
-pdf_files = [file for file in os.listdir(printables_folder) if file.endswith(".pdf")]
+pdf_files = [file for file in os.listdir(
+    printables_folder) if file.endswith(".pdf")]
 
 print("Files in 'printables' folder:")
 for file in pdf_files:
     print(file)
 
-user_input = input("Are these the files that you wish to print? (yes/no): ").lower()
+user_input = input(
+    "Are these the files that you wish to print? (yes/no): ").lower()
 
 if user_input == "yes":
     TF_ACCOUNT_NAME = os.getenv("TF_ACCOUNT_NAME")
@@ -24,13 +26,14 @@ if user_input == "yes":
         subprocess.run(scp_command, shell=True, check=True)
 
         ssh_command = f"sshpass -p '{TF_ACCOUNT_PASSWORD}' ssh {TF_ACCOUNT_NAME}@login.informatik.uni-freiburg.de "
-        lpr_command = f"lpr -o sides=one-sided {file} -P {PRINTER_NAME}"
+        lpr_command = f"lp -o sides=one-sided {file} -d {PRINTER_NAME}"
 
         subprocess.run(ssh_command + lpr_command, shell=True, check=True)
 
         lpq_command = f"{ssh_command} lpq -P {PRINTER_NAME}"
-        lpq_result = subprocess.run(lpq_command, shell=True, capture_output=True, text=True)
-        
+        lpq_result = subprocess.run(
+            lpq_command, shell=True, capture_output=True, text=True)
+
         print("\nlpq result:")
         print(lpq_result.stdout)
 
